2a263432b71b94544bcb330920a2199bbdcc63bc
ylhu <1565918412@qq.com>
2024-06-02 17:48:05 +0800

    Initial commit

---
+++ code/graph.py
@@ -0,0 +1,238 @@
+import os
+import re
+import random
+import graphviz
+
+
+class Graph:
+    def __init__(self) -> None:
+        self.graph_dict = {}
+
+    def __str__(self) -> str:
+        graph_str = ''
+        for (k, v) in self.graph_dict.items():
+            graph_str += k + ' -> '
+            for (name, weight) in v.items():
+                graph_str += f'{name}: {weight}, '
+            graph_str += '\n'
+        return graph_str
+
+    def add_vertex(self, name: str):
+        if name not in self.graph_dict.keys():
+            self.graph_dict[name] = {}
+
+    def add_edge(self, from_name: str, to_name: str):
+        self.add_vertex(from_name)
+        self.add_vertex(to_name)
+        if to_name not in self.graph_dict[from_name].keys():
+            self.graph_dict[from_name][to_name] = 0
+        self.graph_dict[from_name][to_name] += 1
+
+    def get_vertexs(self):
+        return self.graph_dict.keys()
+    
+    def get_edges(self):
+        edges = []
+        for (k, v) in self.graph_dict.items():
+            for (name, weight) in v.items():
+                edges.append((k, name, weight))
+        return edges
+    
+    def get_bridge(self, v1, v2):
+        if v1 not in self.graph_dict.keys():
+            raise KeyError('not exist vertex', v1)
+        if v2 not in self.graph_dict.keys():
+            raise KeyError('not exist vertex', v2)
+        bridge = []
+        for mid in self.graph_dict[v1].keys():
+            for v in self.graph_dict[mid].keys():
+                if v == v2:
+                    bridge.append(mid)
+                    break
+        return bridge
+
+    def _dijkstra(self, from_v):
+        dijkstra_data = {
+            v: {
+                'visited': False, 
+                'distance': float('inf'), 
+                'prev': None
+            } for v in self.graph_dict.keys()
+        }
+        dijkstra_data[from_v]['distance'] = 0
+        while True:
+            lowest_dis = float('inf')
+            lowest_v = None
+            for v in self.graph_dict.keys():
+                if dijkstra_data[v]['visited']:
+                    continue
+                if dijkstra_data[v]['distance'] < lowest_dis:
+                    lowest_dis = dijkstra_data[v]['distance']
+                    lowest_v = v
+            if lowest_v is None:
+                break
+            dijkstra_data[lowest_v]['visited'] = True
+            if all([d['visited'] for d in dijkstra_data.values()]):
+                break
+            for v, weight in self.graph_dict[lowest_v].items():
+                if dijkstra_data[v]['visited']:
+                    continue
+                if lowest_dis + weight < dijkstra_data[v]['distance']:
+                    dijkstra_data[v]['distance'] = lowest_dis + weight
+                    dijkstra_data[v]['prev'] = lowest_v
+        return dijkstra_data
+    
+    def shortest_path(self, v1, v2):
+        if v1 not in self.graph_dict.keys():
+            raise KeyError('not exist vertex', v1)
+        if v2 not in self.graph_dict.keys():
+            raise KeyError('not exist vertex', v2)
+        dijk = self._dijkstra(v1)
+        prev = dijk[v2]['prev']
+        path = [v2]
+        while prev is not None:
+            path.append(prev)
+            prev = dijk[prev]['prev']
+        path.reverse()
+        return dijk[v2]['distance'], path
+    
+    def shortest_all(self, v):
+        if v not in self.graph_dict.keys():
+            raise KeyError('not exist vertex', v)
+        dijk = self._dijkstra(v)
+        shortest = {
+            v2: {
+                'distance': dijk[v2]['distance'], 
+                'path': None
+            } for v2 in self.graph_dict.keys()
+        }
+        for v2 in self.graph_dict.keys():
+            prev = dijk[v2]['prev']
+            path = [v2]
+            while prev is not None:
+                path.append(prev)
+                prev = dijk[prev]['prev']
+            path.reverse()
+            shortest[v2]['path'] = path
+        return shortest
+
+    def random_walk(self):
+        v = random.choice(list(self.graph_dict.keys()))
+        path = []
+        while len(path)<2 or not (path[-1], v) in zip(path[:-1], path[1:]):
+            path.append(v)
+            if len(self.graph_dict[v].keys()) == 0:
+                break
+            v = random.choice(list(self.graph_dict[v].keys()))
+        return path
+
+
+class WordGraph:
+    def __init__(self, txt: str) -> None:
+        self.color_list = ['blue', 'green', 'purple', 'red']
+        self.graph = Graph()
+        words = re.findall('[a-z]+', txt.lower())
+        for i in range(len(words) - 1):
+            self.graph.add_edge(words[i], words[i+1])
+
+    def __str__(self) -> str:
+        return self.graph.__str__()
+
+    def showDirectedGraph(self):
+        draw_graph = graphviz.Digraph()
+        draw_graph.graph_attr['dpi'] = '350'
+        for v in self.graph.get_vertexs():
+            draw_graph.node(v)
+        for (start, end, weight) in self.graph.get_edges():
+            draw_graph.edge(start, end, str(weight))
+        draw_graph.render(os.path.join('output', 'graph'), format='jpg', cleanup=True)
+
+    def queryBridgeWords(self, word1, word2):
+        try:
+            bridge_words = self.graph.get_bridge(word1, word2)
+        except KeyError as e:
+            return f'No {e.args[-1]} in the graph!'
+        if len(bridge_words) == 0:
+            return f'No bridge words from \"{word1}\" to \"{word2}\"!'
+        elif len(bridge_words) == 1:
+            return f'The bridge words from \"{word1}\" to \"{word2}\" is: {bridge_words[0]}.'
+        else:
+            return f'The bridge words from \"{word1}\" to \"{word2}\" are: {", ".join(bridge_words[:-1])} and {bridge_words[-1]}.'
+
+    def generateNewText(self, inputText):
+        if inputText is None:
+            return 'No input text!'
+        words = re.findall('[a-z]+', inputText.lower())
+        added = 0
+        for idx, (word1, word2) in enumerate(zip(words[:-1], words[1:])):
+            try:
+                bridge_words = self.graph.get_bridge(word1, word2)
+            except KeyError as e:
+                continue
+            if len(bridge_words) != 0:
+                words.insert(idx+added+1, random.choice(bridge_words))
+                added+=1
+        return ' '.join(words)
+
+    def calcShortestPath(self, word1, word2):
+        try:
+            distance, path = self.graph.shortest_path(word1, word2)
+        except KeyError as e:
+            return f'not exist word \"{e.args[-1]}\"'
+        draw_graph = graphviz.Digraph()
+        draw_graph.graph_attr['dpi'] = '350'
+        color = random.choice(self.color_list)
+        for v in self.graph.get_vertexs():
+            draw_graph.node(v)
+        for (start, end, weight) in self.graph.get_edges():
+            if (start, end) in zip(path[:-1], path[1:]):
+                draw_graph.edge(start, end, str(weight), color = color)
+            else:
+                draw_graph.edge(start, end, str(weight))
+        draw_graph.render(os.path.join('output', 'shortest_path'), format='jpg', cleanup=True)
+        if distance == float('inf'):
+            return f'\"{word1}\" cannot goto \"{word2}\"'
+        return f'distance: {distance}, path: {"->".join(path)}'
+    
+    def calcShortestPathAll(self, word):
+        try:
+            short_data = self.graph.shortest_all(word)
+        except KeyError as e:
+            return f'not exist word \"{e.args[-1]}\"'
+        out_txt = ''
+        for id, w2 in enumerate(self.graph.get_vertexs()):
+            if short_data[w2]['distance'] == float('inf'):
+                continue
+            draw_graph = graphviz.Digraph()
+            draw_graph.graph_attr['dpi'] = '350'
+            color = random.choice(self.color_list)
+            for v in self.graph.get_vertexs():
+                draw_graph.node(v)
+            for (start, end, weight) in self.graph.get_edges():
+                if (start, end) in zip(short_data[w2]['path'][:-1], short_data[w2]['path'][1:]):
+                    draw_graph.edge(start, end, str(weight), color = color)
+                else:
+                    draw_graph.edge(start, end, str(weight))
+            draw_graph.render(os.path.join('output', f'shortest_path_{id}'), format='jpg', cleanup=True)
+            out_txt += f'{word} to {w2}: \n    distance: {short_data[w2]["distance"]}, path: {"->".join(short_data[w2]["path"])}\n'
+        return out_txt
+
+    def randomWalk(self):
+        path = self.graph.random_walk()
+        txt = ' '.join(path)
+        with open(os.path.join('output', 'walk.txt'), 'w+') as f:
+            f.write(txt)
+        return txt
+
+
+if __name__ == "__main__":
+    # string = 'To explore strange new worlds,\nTo seek out new life and new civilizations'
+    string = 'a to b a from b a go b a back b c d e'
+    g=WordGraph(string)
+    print(g)
+    g.showDirectedGraph()
+    print(g.queryBridgeWords('a', 'b'))
+    print(g.generateNewText('a b a b a b a b'))
+    print(g.calcShortestPath('c','e'))
+    print(g.calcShortestPathAll('d'))
+    print(g.randomWalk())
---
+++ code/main.py
@@ -0,0 +1,189 @@
+import os
+from tkinter import *
+from tkinter import ttk, messagebox, filedialog, simpledialog
+from matplotlib import pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+from graph import WordGraph
+
+
+def cli():
+    txt = input("输入文本: ")
+    graph = WordGraph(txt)
+    while True:
+        choice = int(input("""
+操作列表: 
+        1. 展示有向图
+        2. 查询桥接词
+        3. 生成新文本
+        4. 计算最短路径
+        5. 随机游走
+        6. 退出
+输入操作: """))
+        if choice == 1:
+            graph.showDirectedGraph()
+            pic = plt.imread(os.path.join('output', 'graph.jpg'))
+            plt.imshow(pic)
+            plt.axis('off')
+            plt.show()
+        elif choice == 2:
+            w1 = input('word1: ')
+            w2 = input('word2: ')
+            print(graph.queryBridgeWords(w1, w2))
+        elif choice == 3:
+            txt1 = input("输入文本: ")
+            print(graph.generateNewText(txt1))
+        elif choice == 4:
+            w1 = input('word1: ')
+            w2 = input('word2(输入#计算word1到所有单词的最短路径): ')
+            if w2 != '#':
+                print(graph.calcShortestPath(w1, w2))
+                pic = plt.imread(os.path.join('output', 'shortest_path.jpg'))
+                plt.imshow(pic)
+                plt.axis('off')
+                plt.show()
+            else:
+                print(graph.calcShortestPathAll(w1))
+        elif choice == 5:
+            print(graph.randomWalk())
+        elif choice == 6:
+            break
+
+
+class WordGraphApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Word Graph App")
+        self.create_widgets()
+        # 添加选择文件的功能
+        file_path = filedialog.askopenfilename(initialdir=os.getcwd(), title="Select Text File", filetypes=[("Text files", "*.txt")])
+        if file_path == '':
+            # 如果用户没有选择文件，则退出应用程序
+            messagebox.showwarning("Warning", "No text file selected. Closing the application.")
+            self.root.destroy()
+            exit()
+        with open(file_path, 'r', encoding='utf-8') as f:
+            txt = f.read()
+        self.graph = WordGraph(txt)
+            
+    
+    def create_widgets(self):
+        frame = ttk.Frame(self.root, padding="10 10 10 10")
+        frame.grid(row=0, column=0, sticky=(N, W, E, S))
+        
+        self.text_output = Text(frame, wrap="word", height=10, width=60, font=("Helvetica", 12))
+        self.text_output.grid(row=0, column=0, columnspan=2, padx=5, pady=5, sticky=(N, S, E, W))
+        
+        scrollbar = ttk.Scrollbar(frame, orient=VERTICAL, command=self.text_output.yview)
+        scrollbar.grid(row=0, column=2, sticky=(N, S))
+        self.text_output['yscrollcommand'] = scrollbar.set
+        
+        button_frame = ttk.LabelFrame(frame, text="Operations", padding="10 10 10 10")
+        button_frame.grid(row=1, column=0, columnspan=3, padx=5, pady=5, sticky=(N, S, E, W))
+        
+        ttk.Button(button_frame, text="Show Directed Graph", command=self.show_directed_graph).grid(row=0, column=0, padx=5, pady=5)
+        ttk.Button(button_frame, text="Query Bridge Words", command=self.query_bridge_words).grid(row=0, column=1, padx=5, pady=5)
+        ttk.Button(button_frame, text="Generate New Text", command=self.generate_new_text).grid(row=1, column=0, padx=5, pady=5)
+        ttk.Button(button_frame, text="Calculate Shortest Path", command=self.calc_shortest_path).grid(row=1, column=1, padx=5, pady=5)
+        ttk.Button(button_frame, text="Random Walk", command=self.random_walk).grid(row=2, column=0, padx=5, pady=5)
+        ttk.Button(button_frame, text="Quit", command=self.quit).grid(row=2, column=1, padx=5, pady=5)
+        
+        self.image_frame = ttk.Frame(self.root, padding="10 10 10 10")
+        self.image_frame.grid(row=2, column=0, sticky=(N, W, E, S))
+        
+        self.image_index = 0
+        self.image_paths = []
+
+    def show_directed_graph(self):
+        self.graph.showDirectedGraph()
+        img_path = os.path.join('output', 'graph.jpg')
+        self.show_image(img_path)
+    
+    def query_bridge_words(self):
+        w1 = self.prompt_user("word1")
+        w2 = self.prompt_user("word2")
+        result = self.graph.queryBridgeWords(w1, w2)
+        self.display_output(result)
+    
+    def generate_new_text(self):
+        txt1 = self.prompt_user("Input Text")
+        result = self.graph.generateNewText(txt1)
+        self.display_output(result)
+    
+    def calc_shortest_path(self):
+        w1 = self.prompt_user("word1")
+        w2 = self.prompt_user("word2 (Enter # to calculate the shortest path from word1 to all words)")
+        if w2 != '#':
+            result = self.graph.calcShortestPath(w1, w2)
+            self.display_output(result)
+            img_path = os.path.join('output', 'shortest_path.jpg')
+            self.show_image(img_path)
+        else:
+            result = self.graph.calcShortestPathAll(w1)
+            self.display_output(result)
+            num = (len(result.split('\n')) - 1) // 2
+            # 展示所有最短路径图片
+            self.image_paths = [os.path.join('output', f'shortest_path_{i}.jpg') for i in range(num)]
+            self.image_index = 1
+            self.show_image(self.image_paths[self.image_index])
+            self.create_navigation_buttons()
+    
+    def random_walk(self):
+        result = self.graph.randomWalk()
+        self.display_output(result)
+    
+    def prompt_user(self, prompt):
+        return simpledialog.askstring("Input", prompt)
+    
+    def display_output(self, text):
+        self.text_output.delete(1.0, END)
+        self.text_output.insert(END, text)
+    
+    def show_image(self, img_path):
+        img = plt.imread(img_path)
+        fig, ax = plt.subplots()
+        ax.imshow(img)
+        ax.axis('off')
+        
+        # 清除之前的 canvas
+        for widget in self.image_frame.winfo_children():
+            widget.destroy()
+        
+        self.canvas = FigureCanvasTkAgg(fig, master=self.image_frame)
+        self.canvas.draw()
+        self.canvas.get_tk_widget().grid(row=0, column=0, sticky=(N, W, E, S))
+        
+        self.image_frame.grid_rowconfigure(0, weight=1)
+        self.image_frame.grid_columnconfigure(0, weight=1)
+    
+    def create_navigation_buttons(self):
+        # 添加导航按钮
+        nav_frame = ttk.Frame(self.image_frame, padding="10 10 10 10")
+        nav_frame.grid(row=1, column=0, sticky=(E, W))
+        
+        ttk.Button(nav_frame, text="Previous", command=self.show_previous_image).grid(row=0, column=0, padx=5, pady=5)
+        ttk.Button(nav_frame, text="Next", command=self.show_next_image).grid(row=0, column=1, padx=5, pady=5)
+    
+    def show_next_image(self):
+        if self.image_paths and self.image_index < len(self.image_paths) - 1:
+            self.image_index += 1
+            self.show_image(self.image_paths[self.image_index])
+            self.create_navigation_buttons()
+    
+    def show_previous_image(self):
+        if self.image_paths and self.image_index > 0:
+            self.image_index -= 1
+            self.show_image(self.image_paths[self.image_index])
+            self.create_navigation_buttons()
+            
+    def quit(self):
+        # 清除 output/ 下的所有图片
+        for f in os.listdir('output'):
+            if f.endswith('.jpg'):
+                os.remove(os.path.join('output', f))
+        self.root.quit()
+
+if __name__ == '__main__':
+    root = Tk()
+    app = WordGraphApp(root)
+    root.mainloop()
